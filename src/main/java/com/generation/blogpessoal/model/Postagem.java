454dfeaba04b0ee756ace7e9a5a976d381272174
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-05-18 21:07:54 -0300

    Revisão de conteúdo 16/05

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,9 +1,7 @@
 package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
-
 import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,19 +11,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="tb_postagens")
+@Table(name = "tb_postagens")
 public class Postagem {
 
-	@Id // indica que este atributo será uma chave primária na minha tabela
-	@GeneratedValue(strategy = GenerationType.IDENTITY)//indica que terá um auto-increment
+	@Id //indica que este atributo será uma chave primária na minha tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //indica que a chave primária será gerada junto com um auto-increment
 	private Long id;
 	
 	@NotBlank(message = "Este atributo é de preenchimento obrigatório")
-	@Size(min = 5, max=100,message="Este atributo tem que ter no mínimo 5 caracteres e no máximo 100 caracteres")
+	@Size(min = 5,max = 100,message = "Este atributo precisa ter no mínimo 5 caracteres, e no máximo 100 caracteres")
 	private String titulo;
 	
 	@NotBlank(message = "Este atributo é de preenchimento obrigatório")
-	@Size(min = 10, max=1000,message="Este atributo tem que ter no mínimo 10 caracteres e no máximo 1000 caracteres")
+	@Size(min = 10, max = 1000,message = "Este atributo precisa ter no mínimo 10 caracteres e no máximo 1000 caracteres")
 	private String texto;
 	
 	@UpdateTimestamp
